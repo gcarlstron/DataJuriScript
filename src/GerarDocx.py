5eeae5e8cea517fac73322c3705f68063ce2580e
Gustavo Pacheco Padre Carlstron <gustavo.padre@tradeback.com.br>
2024-12-06 10:53:46 -0300

    WIP

---
+++ src/GerarDocx.py
@@ -0,0 +1,66 @@
+import markdown
+from io import BytesIO
+from bs4 import BeautifulSoup
+from docx.shared import RGBColor
+from docx import Document
+
+
+class GerarDocx:
+
+    def adicionar_tabela_docx(self, doc, dados_tabela: dict[str, str]):
+        for para in doc.paragraphs:
+            if '[[ tabela ]]' in para.text:
+
+                para.text = para.text.replace('[[ tabela ]]', '')
+                tabela = doc.add_table(rows=len(dados_tabela) + 1, cols=2)
+                tabela.style = 'Table Grid'
+
+                cabecalhos = ['Campo', 'Valor']
+                for i, cabecalho in enumerate(cabecalhos):
+                    tabela.cell(0, i).text = cabecalho
+
+                for i, dicionario in enumerate(dados_tabela):
+                    for chave, valor in dicionario.items():
+                        tabela.cell(i, 0).text = chave
+                        tabela.cell(i, 1).text = valor
+
+                tabela.add_row()
+                break
+
+    def convert_markdown_to_docx(self, markdown_file, output_file, dados_tabela, styles=None):
+
+        with open(markdown_file, 'r', encoding='utf-8') as file:
+            documento = file.read()
+
+        doc = Document()
+        html = markdown.markdown(documento)
+        soup = BeautifulSoup(html, 'html.parser')
+
+        for element in soup.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ul', 'ol']):
+            if element.name.startswith('h'):
+                p = doc.add_paragraph(element.text)
+                p.style = f'Heading {element.name[1]}'
+            elif element.name == 'p':
+                if '[[ tabela ]]' in element.text:
+                    doc.add_paragraph(element.text)
+                    self.adicionar_tabela_docx(doc, dados_tabela)
+                else:
+                    for child in element.children:
+                        p = doc.add_paragraph()
+                        if isinstance(child, str):
+                            run = p.add_run(child)
+                        else:
+                            run = p.add_run(child.text)
+                            if child.name == 'strong':
+                                run.bold = True
+                            elif child.name == 'em':
+                                run.italic = True
+                            elif child.name == 'a':
+                                run.font.color.rgb = RGBColor(0, 0, 255)
+                                run.underline = True
+            elif element.name in ['ul', 'ol']:
+                for li in element.find_all('li'):
+                    estilo = 'List Bullet' if element.name == 'ul' else 'List Number'
+                    doc.add_paragraph(li.text, style=estilo)
+
+        doc.save(output_file)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,9 @@
 import json
-import markdown
-from io import BytesIO
-from bs4 import BeautifulSoup
-from docx.shared import RGBColor
 from dotenv import load_dotenv
-from docx import Document
+from src import GerarDocx as g
 from src.DataJuriAuthenticate import DataJuriAuthenticate
 from src.DataJuriClient import DataJuriClient
+from src.GerarDocx import GerarDocx
 from src.PreencherTemplate import arquivo_md
 
 
@@ -24,7 +21,8 @@ def main():
         # Busca e preenche o template
         dados = client.preencher_template(PROCESSO_ID)
         documento, dados_tabela = arquivo_md('../template/retirement-request-template.md', dados)
-        convert_markdown_to_docx(f'{documento}', documento.replace(".md", ".docx"), dados_tabela)
+        docx = GerarDocx()
+        docx.convert_markdown_to_docx(f'{documento}', documento.replace(".md", ".docx"), dados_tabela)
         
         # Salva o resultado em um arquivo
         with open('../template/dados_template.json', 'w', encoding='utf-8') as f:
@@ -36,69 +34,6 @@ def main():
         
     except Exception as e:
         print(f"Erro: {e}")
-
-
-def adicionar_tabela_docx(doc, dados_tabela: dict[str, str]):
-    for para in doc.paragraphs:
-        if '[[ tabela ]]' in para.text:
-
-            para.text = para.text.replace('[[ tabela ]]', '')
-            tabela = doc.add_table(rows=len(dados_tabela) + 1, cols=2)
-            tabela.style = 'Table Grid'
-
-            cabecalhos = ['Campo', 'Valor']
-            for i, cabecalho in enumerate(cabecalhos):
-                tabela.cell(0, i).text = cabecalho
-
-            for i, dicionario in enumerate(dados_tabela):
-                for chave, valor in dicionario.items():
-                    tabela.cell(i, 0).text = chave
-                    tabela.cell(i, 1).text = valor
-
-            tabela.add_row()
-            break
-
-
-def convert_markdown_to_docx(markdown_file, output_file, dados_tabela, styles=None):
-
-    with open(markdown_file, 'r', encoding='utf-8') as file:
-        documento = file.read()
-
-    doc = Document()
-    html = markdown.markdown(documento)
-    soup = BeautifulSoup(html, 'html.parser')
-
-    for element in soup.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ul', 'ol']):
-        if element.name.startswith('h'):
-            p = doc.add_paragraph(element.text)
-            p.style = f'Heading {element.name[1]}'
-        elif element.name == 'p':
-            if '[[ tabela ]]' in element.text:
-                # Adicionar a tabela
-                adicionar_tabela_docx(doc, dados_tabela)
-            else:
-                p = doc.add_paragraph()
-                for child in element.children:
-                    if isinstance(child, str):
-                        run = p.add_run(child)
-                    else:
-                        run = p.add_run(child.text)
-                        if child.name == 'strong':
-                            run.bold = True
-                        elif child.name == 'em':
-                            run.italic = True
-                        elif child.name == 'a':
-                            run.font.color.rgb = RGBColor(0, 0, 255)
-                            run.underline = True
-        elif element.name in ['ul', 'ol']:
-            for li in element.find_all('li'):
-                estilo = 'List Bullet' if element.name == 'ul' else 'List Number'
-                doc.add_paragraph(li.text, style=estilo)
-
-    file_stream = BytesIO()
-    doc.save(file_stream)
-    file_stream.seek(0)
-    return file_stream.getvalue()
 
 
 if __name__ == "__main__":
